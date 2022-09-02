f953955bf43f634cff6072b29659111b6fdca4b0
Andrea Bueide <andrea@abueide.com>
2022-09-02 17:37:23 -0500

    Remove ubild files

--- build/bench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrea/projects/relic/bench
-# Build directory: /home/andrea/projects/relic/build/bench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
