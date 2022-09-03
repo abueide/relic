01bf966b3115e3a90d30669230af8908414f3d97
Andrea Bueide <andrea@abueide.com>
2022-09-02 21:48:46 -0500

    multiplatform

--- flake.nix
+++ flake.nix
@@ -3,18 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: {
-            defaultPackage.x86_64-linux =
-                with import nixpkgs { system = "x86_64-linux"; };
-                    stdenv.mkDerivation {
-                    pname = "relic";
-                    version = "0.5.0";
-                    src = self;
-                    nativeBuildInputs = [ pkgs.cmake pkgs.gmp ];
-                    enableParallelBuilding = true;
-                    cmakeFlags = [ "-DARITH=gmp"];
-            };
-  };
-}
+    outputs = { self, nixpkgs, flake-utils }:
+      flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        rec {
+          defaultPackage = pkgs.stdenv.mkDerivation {
+            pname = "relic";
+            version = "0.5.0";
+            src = self;
+            nativeBuildInputs = with pkgs; [ cmake gmp ];
+            enableParallelBuilding = true;
+            cmakeFlags = [ "-DARITH=gmp"];
+          };
+        }
+      );
+  }
